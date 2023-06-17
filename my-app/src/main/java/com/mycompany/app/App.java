0059caf04fae1fcb8556028a1ad6bd7f70ee03aa
Prakarsh <prakarsh.saffron@gmail.com>
2023-06-17 12:53:23 +0530

    Updated hello file

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,5 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
@@ -12,18 +8,6 @@ public class App
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
-        /**
-         * Checker prevents this from compiling...
-         */
-//        System.out.println("myObject: " + myObject.toString());
-        /**
-         * ... which is simply fantastic. It shows:
-         *
-         * error: [dereference.of.nullable] dereference of possibly-null
-         * reference myObject
-         *
-         * http://checkerframework.org
-         */
         System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
     }
 }
